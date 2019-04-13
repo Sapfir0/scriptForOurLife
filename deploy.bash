341550389def28e283ed8a86ef817c696b20e000
Sapfir0 <sapfir999999@yandex.ru>
2019-04-13 15:01:21 +0000

    1/2

--- deploy.bash
+++ deploy.bash
@@ -9,19 +9,19 @@ fi
 #	./setLocale.bash
 #fi
 
-dir=$(pwd)|grep -oP 'metida'
-if [[$dir != metida]];then 
-	if [[$dir != metida-*]];then
-		echo "You are not in metida project\nSearching metida"
-		VAR=find ~/ -type d -name 'metida*'
+met=$(pwd|grep -oP 'metida') #if 
+echo $met
+if [[ -z "$met" ]] ; then # $met == null
+	if [[ "$met" != "metida*" ]];then
+		echo "You are not in metida project. Searching metida..."
+		VAR=$(find ~/ -type d -name 'metida*')
 		echo "Metida is found in $VAR"
 		cd $VAR
-		
-		if [[ -z "$VAR" ]]; then
+		if [[ -z $VAR ]];then
 			echo "Metida didnt found. Download?"
 			read item
-			case "$item" in 
-				n|N) 
+			case $item in
+				n|N)
 					echo ":("
 					exit -1
 					;;
@@ -29,39 +29,41 @@ if [[$dir != metida]];then
 					echo "Downloading..."
 					git clone https://github.com/avdosev/metida.git
 					;;
+			esac
 		fi
 	fi
 fi
 
-cp autorunServer.bash /etc/init.d/autorunServer.bash
-chmod ugo+x /etc/init.d/autorunServer.bash
-update-rc.d autorunServer.bash defaults #rewrite this 3 rows
+#cp autorunServer.bash /etc/init.d/autorunServer.bash
+#chmod ugo+x /etc/init.d/autorunServer.bash
+#update-rc.d autorunServer.bash defaults #rewrite this 3 rows
 
 #mini CL lol
 lastVersion=$(git log --pretty=format:"%h" -1) #print index of last commit
-
-./autoRunServer.bash $lastVersion
+echo "Последняя версия metida - $lastVersion"
+bash ./autorunServer.bash $lastVersion
 
 #working with docker
-apt update
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-apt update && apt-cache policy docker-engine
 
-packages = {
-	nodejs, npm, mysql-server, docker-engine }
+#apt update
+#apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+#apt update && apt-cache policy docker-engine
 
-apt install -y packages
+#packages = {
+#	nodejs, npm, mysql-server, docker-engine }
+
+#apt install -y $packages
 
 #need create special account for working with DB
-mysql_secure_installation
+#mysql_secure_installation
 
-mysql -u whoami -p1 <<EOF
-create database usersDB; 
-create table usersDB.users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(50), login VARCHAR(25), password VARCHAR(100) created_at datetime, updated_at datetime);
-create user 'metidaSQL'@'localhost' identified by '1234';
-grant all privileges on usersDB.users to 'metidaSQL'@'localhost';
-alter user 'metidaSQL'@'localhost' identified WITH mysql_native_password BY '1234';
-EOF
+#mysql -u whoami -p1 <<EOF
+#create database usersDB; 
+#create table usersDB.users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(50), login VARCHAR(25), password VARCHAR(100) created_at datetime, updated_at datetime);
+#create user 'metidaSQL'@'localhost' identified by '1234';
+#grant all privileges on usersDB.users to 'metidaSQL'@'localhost';
+#alter user 'metidaSQL'@'localhost' identified WITH mysql_native_password BY '1234';
+#EOF
 
 
