29fce49bed0324020501f3d3ece66b6cc67ac006
Sapfir0 <sapfir999999@yandex.ru>
2019-05-11 14:24:24 +0400

    Добалвен echo

---
+++ browserHistoryDeleter/Mozilla.py
@@ -0,0 +1,57 @@
+import os
+import sqlite3
+
+from config import urls, titles
+
+class Mozilla:
+    def __init__(self):
+        userName = os.getlogin() 
+        path = os.path.join("C:",os.sep,"Users", userName , "AppData", "Roaming","Mozilla","Firefox","Profiles")
+        profile = os.listdir(path) 
+
+        if ( len(profile) > 1): #не уверен, что сработает
+            print("A lots of profile, will be selected first")
+
+        firefox_places = os.path.join("C:",os.sep,"Users", userName , "AppData", "Roaming","Mozilla","Firefox","Profiles", str(profile[0]) ,"places.sqlite")
+        #блок конфигов окончен
+
+        self.connection = sqlite3.connect(firefox_places)
+        self.cursor = self.connection.cursor()
+
+        self.checkBadSites()
+
+
+    def deleteRecords(self, object, typeOfDeleter ):
+        urlOrTitle = self.isCountOfIteration(typeOfDeleter)
+
+        sql = "DELETE FROM moz_places WHERE " + urlOrTitle + " LIKE " + object + ";"
+        self.cursor.execute(sql)
+        self.connection.commit()
+
+    def checkBadSites(self):
+        data = [ urls, titles ] 
+        i=0
+        while (i < len(data) ):
+            for j in data[i]:
+                urlOrTitle = self.isCountOfIteration(i)
+                sqlItemJ = "'%"+j+"%'"
+                sql = "SELECT url FROM moz_places WHERE " + urlOrTitle + " LIKE " + sqlItemJ  + ";"
+                print("Searching bad item... " + j )
+                self.cursor.execute(sql)
+                if (self.cursor.fetchall() != [] ):
+                    print("Deleting bad history " + j)
+                    self.deleteRecords(sqlItemJ, i)
+                else:
+                    print("History is clear")
+            i+=1
+
+    def isCountOfIteration(self,i):
+        if (i == 0):
+            urlOrTitle="url"
+        elif (i == 1):
+            urlOrTitle="title"
+        else:
+            urlOrTitle=None
+        return urlOrTitle
+
+    
