041b100b05b5d4b91df73efaee032b035ba7c745
Sapfir0 <sapfir999999@yandex.ru>
2019-05-12 16:12:05 +0400

    j

--- VSTU_Cluster/baseCommand.sh
+++ VSTU_Cluster/baseCommand.sh
@@ -13,22 +13,40 @@ function disconnect() {
     exit
 }
 
+function checkGetPushArgument() {
+    if [[ -z "$1"  ]];then
+        echo "First argument is null"
+        return -1
+    fi
+    if [[ -z "$2"  ]];then
+        echo "Second argument is null, but will be used current path"
+        outputPath = "./"
+    fi
+    return outputPath
+}
 
 function pushToServer() { #$1 - что постить(локалка), $2, куда постить(серв)
-    scp -P $port $1 $loginOnCluster$clustersUrl:$2
+    echo $(checkGetPushArgument "$1" "$2")
+    outputPath = $(checkGetPushArgument "$1" "$2")
+
+    scp -P $port "$1" "$loginOnCluster""$clustersUrl":"$outputPath"
 }
 
 function getFromServer() { #$1 - что получаем(сервер), $2 - куда получаем(локалка)
-    scp -P $port $loginOnCluster$clustersUrl:$1 $2
+    outputPath = "$(checkGetPushArgument "$1" "$2")"
+    echo $outputPath
+    
+    scp -P $port "$loginOnCluster""$clustersUrl":"$1" "$outputPath"
 }
 
-function connectToNode(){
-    if [[ $1 <30 || $1 > 53 ]]
+function connectToNode()  {
+    if [[ $1 <30 || $1 > 53 ]]; then
         echo "This is undefined node"
         return
+   fi
     ssh node$1
 }
 
 #pushToServer "./../README.md" "~"
-#getFromServer "~/README.md" "./"
+getFromServer 
 #userIntelPhi3
