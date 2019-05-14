38b6162b7268e3cd382751197c673e346677bb8d
Sapfir0 <sapfir999999@yandex.ru>
2019-05-15 00:13:25 +0400

    py

--- VSTU_Cluster/baseCommand.py
+++ VSTU_Cluster/baseCommand.py
@@ -3,34 +3,37 @@ import paramiko
 import configCluster as cfg
 import commandList as cl
 
+
 class VSTU_CLuster:
     def __init__(self):
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())# данная строчка решает проблему с known_hosts
-
 
     def connect(self):
         self.client.connect(hostname=cfg.hostname, username=cfg.username, password=cfg.password, port=cfg.port)
 
     def runCommand(self, command):
         stdin, stdout, stderr = self.client.exec_command(command)
-        data = stdout.read() + stderr.read()
-        print(data)
+        if ( stderr.read() != None):
+            print(stderr.read())
+        print(stdout.read())
 
     def downloadMetida(self):
         self.runCommand(cl.gitClone)
 
     def pushToServer(self, inputDir, outputDir):
          #scp -P $port $1 $loginOnCluster$clustersUrl:$2
-        self.runCommand("scp" + " -P " + cfg.port + " "  +inputDir + " " + cfg.username + cfg.hostname + ":" + outputDir) 
+        self.runCommand("scp" + cl.portSettings + " "  + inputDir + " " + cl.clusterPath + ":" + outputDir) 
     
     def getFromServer(self, inputDir, outputDir):
         # scp -P $port $loginOnCluster$clustersUrl:$1 $2
-        self.runCommand("scp" + " -P " + cfg.port + " " + cfg.username + cfg.hostname + ":" + inputDir + " " + outputDir) 
+        self.runCommand("scp" + cl.portSettings + " " + cl.clusterPath + ":" + inputDir + " " + outputDir) 
 
     def connectToNode(self, nodeNumber):
         self.runCommand("ssh node" + nodeNumber)  #без пробела
 
+    # def workWithNpm(self):
+    #     self.runCommand()
 
     def disconnect(self):
         self.client.close()
--- VSTU_Cluster/commandList.py
+++ VSTU_Cluster/commandList.py
@@ -1,10 +1,19 @@
+import configCluster as cfg
+
 command = [ 
     "ls",
     "git clone https://github.com/avdosev/metida",
+]
+
+gitClone =  "git clone https://github.com/avdosev/metida"
+clusterPath = cfg.username + cfg.hostname
+portSettings = " -P " + cfg.port
+
+npm = [
+    "cd metida",
     "npm i",
     "npm start"
 ]
 
-gitClone =  "git clone https://github.com/avdosev/metida"
 
 
