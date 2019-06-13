025046666efd5babfa9f7bfc99d32fe9bb7d43c3
Sapfir0 <sapfir999999@yandex.ru>
2019-06-13 19:56:26 +0400

    вот

--- scanComputerParams.py
+++ scanComputerParams.py
@@ -4,42 +4,72 @@
 # тут маленькая линукс вики
 
 import os
+import subprocess
+from subprocess import PIPE
+import re
 
-TempofCPUandGPU = "sensors | egrep \"Core|temp|Adapter\" "
-TempOfHDD = "sudo hddtemp /dev/sda" #надо понять как поступать с правами админа
     
-data = os.popen(TempofCPUandGPU).read()
 
-#print(data)
+def getTempOfCPU():
+    TempofCPU = "sensors | egrep \"Core\" "
+    data = os.popen(TempofCPU).read()
+    sum=0.
+    counter = int(os.popen("sensors | egrep 'Core' -o | wc -l").read()) #вернет количество ядер
+    for i in range(0, counter): #второе число - количество совпадений с шаблоном
+        tempOfCore=data.splitlines()[i][16:20].strip()
+        sum+=float(tempOfCore)
+    result = sum / counter
+    print("Average temperature of CPU: " + str(result))
+    return result
 
-
-# Загрузка проца, нужно как-то определить, пока я не сдал физику
-loadCpu = "top | egrep \"%Cpu\" "
-#data = os.popen(loadCpu).read()
-#%Cpu(s):  9,2 us,  2,5 sy,  0,0 ni, 86,4 id,  0,8 wa,  0,0 hi,  1,1 si,  0,0 st
-#пример выдаваемой строки
-# user(время на процессы юзера), system(системные процессы), niced(не приоритетные процессы юзера), idle(вроемя на простой ядра), 
-# в этом случае надо понять как остановить процесс запроса(или взять только текущее значение, а не мониторинг)
-
-#вариант 2
-uptime="uptime"
-data = os.popen(uptime).read()
-# 23:10:39 up  6:19,  1 user,  load average: 1,05, 0,89, 0,79
-# load average: 
-# Где значения от 0 до 1 указывают на не нагруженную систему близкую к простою.
-# Значения от 1 до 10 - как умеренно нагруженную систему. Все нормально.
-# Значения от 10 до 30 - как высоконагруженную систему. Не следует добавлять нагрузку. Можно подумать о поиске оптимизации нагрузки. Оптимизация рекомендуется.
-# Значения от 30 до 100 - как чрезмерно нагруженную систему, например, причиной может быть большая доля iowait из-за перегрузки - большое количество потоков ввода вывода на одно блочное устройство, аномально медленная работа блочного устройства из-за неисправности, другие подобные причины, связанные с возникновением "бутылочного горлышка" в системе, которое надо расшивать - при таких значениях LA - производительность неэффективная. Оптимизация необходима.
-# Значения выше 100 - следует воспринимать как аварийное состоянии системы с точки зрения производительности. Нужно принимать меры безотлагательно.
-# Значения выше 1000 - и дальнейший рост LA ведут к падению ядра, как правило, падение системы происходит в течении ближайших нескольких часов. Требуется экстренная реакция для избежания отказа систем и потери данных.
-
-# ЛИБО (инфа с хабра https://habr.com/ru/post/216827/)
-# если нагрузка больше 1 для одноядерного проца, то все пиздец, ты упал
-# 1 на каждое ядро
-howManyCoreIHave = "cat /proc/cpuinfo | grep \"cpu cores\" "
+def getTempOfDiscs(): 
+    TempOfHDD = "sudo hddtemp /dev/sda" #надо понять как поступать с правами админа
+    data = os.popen(TempOfHDD).read()
+    
+    print("Temperature of HDD:" + data[-6:-4]) #предположим что температура будет выглядтеть таким образом 00.0
 
 
-#также можно юзнуть это
-cpuLoad = "iostat -c | grep \"avg\" -A 1"
+def getLoadOfCpuByUptime():
+    # Загрузка проца, нужно как-то определить, пока я не сдал физику #АХАХАХ
+    #пример выдаваемой строки
+    # user(время на процессы юзера), system(системные процессы), niced(не приоритетные процессы юзера), idle(вроемя на простой ядра), 
+    uptime="uptime"
+    data = os.popen(uptime).read() # 23:10:39 up  6:19,  1 user,  load average: 1,05, 0,89, 0,79
+    digit=re.findall(r"\d+", data)[-6:]
+    print( "Current load: " + digit[0] + "." + digit[1] )
+    print( "5-mintute load: " + digit[2] + "." + digit[3] )
+    print( "15-mintute load: " + digit[4] + "." + digit[5] )
+    return digit
+    # (инфа с хабра https://habr.com/ru/post/216827/)
+    # если нагрузка больше 1 для одноядерного проца, то все пиздец, ты упал
+    # 1 на каждое ядро
+
+
+def getLoadOfCpuByIostat():
+    #также можно юзнуть это
+    cpuLoad = "iostat -c | grep \"avg\" -A 1"
+    data = os.popen(cpuLoad).read()
+    load = data.splitlines()[1]
+    digit=re.findall(r"\d+", load)
+    list=[]
+    for i in range(0, len(digit), 2):
+        list.append( digit[i] + "."  + digit[i+1] ) #avg-cpu:  %user   %nice %system %iowait  %steal   %idle
+    # первые 3 пункта - загруженность проца, остальные - свободные % проца
+    usedResources= float(list[0]) + float(list[1]) + float(list[2])
+        
+    print("CPU load on " + str(usedResources) + "%")
+    return usedResources
+
+
+
+getTempOfDiscs()
+getTempOfCPU()
+getLoadOfCpuByUptime()
+getLoadOfCpuByIostat()
+
+
+
+howManyCoreIHave = "cat /proc/cpuinfo | grep \"cpu cores\" " #есть еще лулзный варик в начале 
+
 
 
