b5c2e46618f022f8b0160b5d8a0ccbf1bb6de5f5
Sapfir0 <sapfir999999@yandex.ru>
2019-08-27 14:10:19 +0400

    Обновление deleter

--- browserHistoryDeleter/Mozilla.py
+++ browserHistoryDeleter/Mozilla.py
@@ -2,39 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import os
+import pwd
 import sqlite3
 from sys import platform
 
-
-if (os.path.exists("config.py")):
+if os.path.exists("config.py"):
     from config import urls, titles
-else:
-    exit
+
 
 def config():
-    if (platform =="win32"):
-        userName = os.getlogin() 
-        path = os.path.join("C:",os.sep,"Users", userName , "AppData", "Roaming","Mozilla","Firefox","Profiles")
-        profile = os.listdir(path) 
+    os.getlogin = lambda: pwd.getpwuid(os.getuid())[0]
+    userName = os.getlogin()
+    if platform == "win32":
+        path = os.path.join("C:", os.sep, "Users", userName, "AppData", "Roaming", "Mozilla", "Firefox", "Profiles")
+        profile = os.listdir(path)
 
-        if ( len(profile) > 1): #не уверен, что сработает
+        if len(profile) > 1:  # не уверен, что сработает
             print("A lots of profile, will be selected first")
 
-        firefox_places = os.path.join("C:",os.sep,"Users", userName , "AppData", "Roaming","Mozilla","Firefox","Profiles", str(profile[0]) ,"places.sqlite")
-
+        firefox_places = os.path.join("C:", os.sep, "Users", userName, "AppData", "Roaming", "Mozilla",
+                                      "Firefox", "Profiles", str(profile[0]), "places.sqlite")
         return firefox_places
-
-    elif (platform == "linux2"):
-
-        path = os.path.join("/", "home", "sapfir", ".mozilla", "firefox", 'profiles.ini')
+    elif platform == "linux" or platform == "linux2":
+        path = os.path.join("/", "home", userName, ".mozilla", "firefox", 'profiles.ini')
         cmd = "cat " + path + " | grep Default | head -1 "
         profileDir = os.popen(cmd).read()[8:]
         profileDir = profileDir.strip()
-        firefox_places = os.path.join("/", "home", "sapfir", ".mozilla", "firefox", profileDir ,"places.sqlite")
+        firefox_places = os.path.join("/", "home", userName, ".mozilla", "firefox", profileDir, "places.sqlite")
         return firefox_places
     else:
         print("Unknown platform")
-        exit
+        exit()
+
 
 class Mozilla:
     def __init__(self):
@@ -45,38 +44,34 @@ class Mozilla:
 
         self.checkBadSites()
 
+    def deleteRecords(self, sqlItemJ, typeOfDeleter):
+        urlOrTitle = isCountOfIteration(typeOfDeleter)
 
-    def deleteRecords(self, object, typeOfDeleter ):
-        urlOrTitle = self.isCountOfIteration(typeOfDeleter)
-
-        sql = "DELETE FROM moz_places WHERE " + urlOrTitle + " LIKE " + object + ";"
+        sql = "DELETE FROM moz_places WHERE " + urlOrTitle + " LIKE " + sqlItemJ + ";"
         self.cursor.execute(sql)
         self.connection.commit()
 
     def checkBadSites(self):
-        data = [ urls, titles ] 
-        i=0
-        while (i < len(data) ):
+        data = [urls, titles]
+        for i, item in enumerate(data):
             for j in data[i]:
-                urlOrTitle = self.isCountOfIteration(i)
-                sqlItemJ = "'%"+j+"%'"
-                sql = "SELECT url FROM moz_places WHERE " + urlOrTitle + " LIKE " + sqlItemJ  + ";"
-                print("Searching bad item... " + j )
+                urlOrTitle = isCountOfIteration(i)
+                sqlItemJ = "'%" + j + "%'"
+                sql = "SELECT url FROM moz_places WHERE " + urlOrTitle + " LIKE " + sqlItemJ + ";"
+                print("Searching bad item... " + j)
                 self.cursor.execute(sql)
-                if (self.cursor.fetchall() != [] ):
+                if self.cursor.fetchall():
                     print("Deleting bad history " + j)
                     self.deleteRecords(sqlItemJ, i)
                 else:
                     print("History is clear")
-            i+=1
 
-    def isCountOfIteration(self,i):
-        if (i == 0):
-            urlOrTitle="url"
-        elif (i == 1):
-            urlOrTitle="title"
-        else:
-            urlOrTitle=None
-        return urlOrTitle
 
-    
+def isCountOfIteration(i):
+    if i == 0:
+        urlOrTitle = "url"
+    elif i == 1:
+        urlOrTitle = "title"
+    else:
+        urlOrTitle = None
+    return urlOrTitle
--- browserHistoryDeleter/deleteHistory.py
+++ browserHistoryDeleter/deleteHistory.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 import os
-
 from Mozilla import Mozilla
 
+
 def configs():
-    if (not os.path.exists("config.py")):
-        basicConfig = [ "urls = [ \"badUrl1\", \"badUrl2\", \"badUrln\" ] \n", "titles = [ \"badTitle1\", \"badTitle2\", \"badTitlen\" ]"  ]
+    if not os.path.exists("config.py"):
+        basicConfig = ["urls = [ \"badUrl1\", \"badUrl2\", \"badUrln\" ] \n",
+                       "titles = [ \"badTitle1\", \"badTitle2\", \"badTitlen\" ]"]
         basicHelper = "\n#Specify the keywords for which the history will be deleted "
         print("Configs file was not found")
         with open('config.py', 'w') as f:
@@ -18,8 +16,8 @@ def configs():
             print("Open config.py for further instructions")
         return False
     return True
-  
-configs()
-m = Mozilla()
 
-    
+
+if __name__ == "__main__":
+    configs()
+    m = Mozilla()
