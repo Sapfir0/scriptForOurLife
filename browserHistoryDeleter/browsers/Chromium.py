073e45dddf49e56fa452eaba233d495b392a7f63
Sapfir0 <sapfir999999@yandex.ru>
2019-08-29 15:56:25 +0400

    readme update

--- browserHistoryDeleter/browsers/Chromium.py
+++ browserHistoryDeleter/browsers/Chromium.py
@@ -13,6 +13,8 @@ class ChromiumSnap(Browser):
     tableWithUrls = "urls"  # тут 2 таблицы, в которых
     tableWithKeywords = 'keyword_search_terms'
 
+    # Shortcuts / select text from omni_box_shortcuts ;
+
     def checkBadSites(self):
         for url in urls:
             if self.selectQuery(self.columnWithUrl, self.tableWithUrls, self.columnWithUrl, url):
@@ -28,6 +30,6 @@ class ChromiumSnap(Browser):
 
     def findDatabaseFile(self):
         databasePath = os.path.join(os.path.expanduser("~"), "snap", "chromium",
-                            "821", ".config", "chromium", "Default", "History")  # TODO жесткое определение профиля 821
+                            "821", ".config", "chromium", "Default", "Shortcuts")  # TODO жесткое определение профиля 821
 
         return databasePath
