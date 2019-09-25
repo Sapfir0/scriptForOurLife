7e2194ea3987e70deded22366370a63e490d01c4
Sapfir0 <sapfir999999@yandex.ru>
2019-09-25 09:35:21 +0400

    foo

--- 100.py
+++ 100.py
@@ -1,16 +1,12 @@
 import random
-import enum
-class sql(enum.Enum):
-    SELECT = "SELECT",
-    UPDATE = "UPDATE",
-    DELETE = "DELETE",
-    INSERT = "INSERT",
-    ZATICHKA = ""
+
 
 tables = {
-    'table1': ['column11', 'column21'],
-    'table2': ['column12', 'column22'] # table: columns
+    'table1': [{'column11':str}, {'column21':int}],
+    'table2': [{'column12':float}, {'column22':str}] 
 }
+# table: [ {column: type} , {column2: type}]
+
 
 insertUpdateData = [1, "foo", 2.4] # можно сделать тупо проверку по типу
 
@@ -18,18 +14,21 @@ for array in tables.values():
     array.append("*")
 
 def abstractComand(command, table='', selectStatement='') -> str:
+    stringCommand = command.value[0]
     if not table:
         table = random.choice(list(tables.keys()))
         operationsColumn = random.choice(tables[table]) # пока допустим что только один оператор
+    if operationsColumn != "*":
+        operationsColumn = list(operationsColumn.keys())[0] # выбираем название столбца
     if command == sql.DELETE:
-        return f"{command.value[0]} from {table}{selectStatement};"
+        return f"{stringCommand} from {table}{selectStatement};"
     elif command == sql.INSERT:
         #values = 
-        return f"{command.value[0]} INTO {table} VALUES ();"
+        return f"{stringCommand} INTO {table} VALUES ();"
     elif command == sql.SELECT:
-        return f"{command.value[0]} {operationsColumn} from {table}{selectStatement};"
+        return f"{stringCommand} {operationsColumn} from {table}{selectStatement};"
     elif command == sql.UPDATE:
-        return f"{command.value[0]} {table} SET {{ }};"
+        return f"{stringCommand} {table} SET {{ }};"
 
 
 
@@ -48,6 +47,19 @@ def insertTemplate():
 def selectTemplate():
     return abstractComand(sql.SELECT)
 
+import enum
+class sql(enum.Enum):
+    SELECT = "SELECT",
+    UPDATE = "UPDATE",
+    DELETE = "DELETE",
+    INSERT = "INSERT",
+    ZATICHKA = ""
 
 funarray = [updateTemplate(), selectTemplate(), deleteTemplate(), insertTemplate()]
 print(random.choice(funarray))
+
+
+def generateWhereStatements():
+    
+        
+    return f"WHERE {}"
