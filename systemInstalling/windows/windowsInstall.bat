0c65ea4c05424914b598aa7a10a54ed7afab158a
Sapfir0 <sapfir999999@yandex.ru>
2020-01-03 18:32:14 +0400

    windows installing

---
+++ systemInstalling/windows/windowsInstall.bat
@@ -0,0 +1,3 @@
+set packages="vlc nodejs wget winscp clion-ide pycharm chromium dotnetcore-sdk vscode wireshark cmake calibre winmerge k-litecodecpackfull steam postman rufus msys2 cpu-z.install mysql androidstudio python3 filezilla netfx-4.7.1-devpack ccleaner vcredist140 putty.install git.install firefox flashplayeractivex adobereader 7zip.install"
+
+choco install -y %packages%
