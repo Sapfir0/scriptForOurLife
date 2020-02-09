b931ce4cec517d6b59c61b225093685ffcf18d68
Sapfir0 <sapfir999999@yandex.ru>
2020-02-09 14:22:35 +0400

    fix windows installing

--- systemInstalling/windows/windowsInstall.bat
+++ systemInstalling/windows/windowsInstall.bat
@@ -1,3 +1,15 @@
-set packages="vlc nodejs wget winscp clion-ide pycharm chromium dotnetcore-sdk vscode wireshark cmake calibre winmerge k-litecodecpackfull steam postman rufus msys2 cpu-z.install mysql androidstudio python3 filezilla netfx-4.7.1-devpack ccleaner vcredist140 putty.install git.install firefox flashplayeractivex adobereader 7zip.install"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+set packages="vlc wget winscp docker-desktop cmake calibre winmerge steam postman rufus msys2 cpu-z.install filezilla ccleaner putty.install git.install adobereader 7zip.install"
+set ide="pycharm phpstorm jetbrains-rider vscode visualstudio2019community"
+set browser="chromium firefox"
+set language="nodejs.install php mysql sqlite python3 jre8"
+set services="netfx-4.7.1-devpack vcredist140 dotnetcore-sdk"
 
 choco install -y %packages%
+choco install -y %ide%
+choco install -y %browser%
+choco install -y %language%
+choco install -y %services%
+
+choco install -y utorrent --ignore-checksums
