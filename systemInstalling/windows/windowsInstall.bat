e17a846157b1ae3d5f95700f86ae7fe3c1773499
Sapfir0 <sapfir999999@yandex.ru>
2020-05-16 00:13:04 +0400

    windows install update

--- systemInstalling/windows/windowsInstall.bat
+++ systemInstalling/windows/windowsInstall.bat
@@ -1,9 +1,9 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-set packages="vlc wget winscp docker-desktop cmake calibre winmerge steam postman rufus msys2 cpu-z.install filezilla ccleaner putty.install git.install adobereader 7zip.install"
-set ide="pycharm phpstorm jetbrains-rider vscode visualstudio2019community"
+set packages="vlc wget docker-desktop cmake steam postman cpu-z.install utorrent gpu-z.install filezilla ccleaner putty.install git.install adobereader 7zip.install"
+set ide="pycharm webstorm jetbrains-rider vscode visualstudio2019community"
 set browser="chromium firefox"
-set language="nodejs.install php mysql sqlite python3 jre8"
+set language="nodejs.install mysql sqlite python3 jre8"
 set services="netfx-4.7.1-devpack vcredist140 dotnetcore-sdk"
 
 choco install -y %packages%
