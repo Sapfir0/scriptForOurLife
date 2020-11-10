a0117735f16cc6e616785f7f2d2e3ed4cac2aa0b
Alexander Yuryev <alexander.yuryev@singularis.local>
2020-11-10 22:59:42 +0400

    Правки для установки винды

--- systemInstalling/windows/windowsInstall.bat
+++ systemInstalling/windows/windowsInstall.bat
@@ -1,9 +1,10 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 
-choco install -y vlc docker-desktop steam postman ccleaner git adobereader 7zip
-choco install -y pycharm webstorm vscode visualstudio2019community
-choco install -y chromium firefox
-choco install -y nodejs.install mysql sqlite python3 jre8
+choco install -y vlc docker-desktop steam postman git adobereader 7zip
+choco install -y vscode visualstudio2019community
+choco install -y chromium firefox discord telergram
+choco install -y nodejs mysql sqlite python3 jre8 yarn heidisql
+choco install -y jetbrainstoolbox  
 
 choco install -y utorrent --ignore-checksums
