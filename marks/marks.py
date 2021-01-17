e20103c06d2743821ba1245f08dc8d7403bbe72f
Sapfir0 <sapfir999999@yandex.ru>
2021-01-17 13:54:55 +0300

    Добавлен рассчет среднего балла

---
+++ marks/marks.py
@@ -0,0 +1,51 @@
+def inInterval(mark, interval):
+    return mark >= interval[0] and mark <= interval[1]
+
+m3 = (61, 75)
+m4 = (76, 89)
+m5 = (77, 90)
+
+
+first = [85, 90, 70, 76,76, 92, 85, 81, 76, ]
+
+second = [64, 52, 64, 86, 76,  100, 90, 88, 85]
+
+third = [76, 96, 90,  76, 76, 84, 93, 76, 93]
+
+thouth = [61, 98, 72,61, 90,68,90,85,96,95]
+
+thith = [100, 91, 88, 95,  78, 90, 80]
+
+sixs = [85, 99, 95,  100, 100, 100, 100, 90]
+
+# sevens = []
+
+allSemesters = [first, second, third, thouth, thith, sixs]
+
+def getAvgIn5Marks():
+    marks5 = []
+    for sem in allSemesters:
+        for mark in sem:
+            if inInterval(mark, m3):
+                marks5.append(3)
+            elif inInterval(mark, m4):
+                marks5.append(4)
+            elif inInterval(mark, m5):
+                marks5.append(5)
+    return sum(marks5) / len(marks5)
+
+
+def getAvg():
+    def avg(lst): 
+        return sum(lst) / len(lst) 
+
+    avgInEachSem = list(map(lambda sem: avg(sem), allSemesters))
+
+    # print(avgInEachSem)
+
+    return avg(avgInEachSem) /100*5
+
+
+print(getAvgIn5Marks())
+print(getAvg())
+
